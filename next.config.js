01707a495f007f78ebea8277c51c39c1a7dec641
VuTan115 <tanboy042001@gmail.com>
2023-07-09 02:03:54 +0700

    init source code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'development',
+  },
+};
 
-module.exports = nextConfig
+const moduleConfig = {
+  pwa: {
+    dest: 'public',
+    // disable: process.env.NODE_ENV === 'development',
+    register: true,
+  },
+};
+
+const withPWA = require('next-pwa')(moduleConfig.pwa);
+
+module.exports = () => {
+  const plugins = [withPWA];
+  const config = plugins.reduce((acc, next) => next(acc), {
+    ...nextConfig,
+  });
+  return config;
+};
+// /** @type {import('next').NextConfig} */
+
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   register: true,
+//   skipWaiting: true,
+// });
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+// });
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,53 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='apple-touch-icon' href='/icon.png'></link>
+        {/* Meta tags */}
+        <meta name='theme-color' content='#f69435' />
+        <meta name='background-color' content='#f69435' />
+        <meta name='display' content='standalone' />
+        <meta name='scope' content='/' />
+        <meta name='start_url' content='/' />
+        <meta name='application-name' content='Quản lý nợ' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+        <meta name='apple-mobile-web-app-title' content='Quản lý nợ' />
+        <meta name='description' content='Ứng dụng quản lý nợ của khách hàng' />
+
+        {/* Link tags */}
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='192x192'
+          href='/icon-192x192.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='256x256'
+          href='/icon-256x256.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='384x384'
+          href='/icon-384x384.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='512x512'
+          href='/icon-512x512.png'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
